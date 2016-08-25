68202e91de4f6aa5e706611462fd48e0bd588820
vasyl.lutsenko <Vasyl.Lutsenko@ext.win-interactive.com>
2016-08-25 05:59:51 +0300

    added spinner for submitting

--- PokemonStore/PokemonStore.Web/App/Ctrl/OrderCtrl.js
+++ PokemonStore/PokemonStore.Web/App/Ctrl/OrderCtrl.js
@@ -3,7 +3,9 @@
         "$scope", "$http", '$location', "ROUTES", "Notification", function ($scope, $http, $location, ROUTES, Notification) {
             $scope.$root.title = 'Pokemon Store | Order';
             $http.defaults.headers.post["Content-Type"] = "application/json";
+            $scope.submitingInProgress = false;
             $scope.order = function (name, email, phone) {
+                $scope.submitingInProgress = true;
                 $http.post(ROUTES.API.OPDER,
                     {
                         name: name,
@@ -13,9 +15,11 @@
                     .then(function (pack) {
                         $scope.orderResult = pack.data;
                         $location.path(ROUTES.UI.ORDERS_FEED);
-                        Notification.success("Pokemon is ordered for you!");
+                        Notification.success("Pokemon is ordered for " + name + " !");
                     }, function () {
                         Notification.error("Error while order Pokemon!");
+                    }).finally(function () {
+                        $scope.submitingInProgress = false;
                     });
             }
         }
